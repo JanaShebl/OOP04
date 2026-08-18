f4a519c76ad04a79776e6ee1ae374cbbc1a994c3
JanaShebl <janamohamed195200@gmail.com>
2026-08-18 03:18:21 +0300

    Q1

--- OOP05/Program.cs
+++ OOP05/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            #region Q1
+
+            // a)  What is Abstraction in Object-Oriented Programming?
+            // abstraction is an OOP concept that allow to hide the implementation of an important details and show the essential features
+
+            // b)  Why is abstraction considered one of the four pillars of OOP?
+            // because it allows to hide the implementation details and show only the essential features of an object, making it easier to understand and reuse
+
+            #endregion
         }
     }
 }
